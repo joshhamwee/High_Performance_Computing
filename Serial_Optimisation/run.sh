33d013232177ac6549286c778a8ca5221bd1050a
Josh Hamwee <joshhamwee@me.com>
2019-10-24 11:39:32 +0100

    Final

--- Serial_Optimisation/run.sh
+++ Serial_Optimisation/run.sh
@@ -2,9 +2,9 @@
 rm stencil
 rm stencil.out
 rm stencil.pgm
-icc -std=c99 -O2 -xHOST -fast stencil.c -o stencil 
+icc -fast -O3 -xHOST -std=c99 stencil.c -o stencil 
 sbatch stencil.job
-sleep 10
-python check.py --ref-stencil-file stencil_1024_1024_100.pgm --stencil-file stencil.pgm
+sleep 20
+python check.py --ref-stencil-file stencil_8000_8000_100.pgm --stencil-file stencil.pgm
 cat stencil.out
 
