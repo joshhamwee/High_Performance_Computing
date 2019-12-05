5ee7b42b03d77bb09b61fa5da9aee4f62c5952a3
Josh Hamwee <joshhamwee@me.com>
2019-12-05 11:22:40 +0000

    Change run.sh

--- MPI_Optimisation/run.sh
+++ MPI_Optimisation/run.sh
@@ -2,7 +2,7 @@
 rm stencil
 rm stencil.out
 rm stencil.pgm
-icc -fast -O3 -xHOST -std=c99 stencil.c -o stencil
+mpicc -std=c99 stencil.c -o stencil
 sbatch stencil.job
 sleep 20
 python check.py --ref-stencil-file stencil_1024_1024_100.pgm --stencil-file stencil.pgm
