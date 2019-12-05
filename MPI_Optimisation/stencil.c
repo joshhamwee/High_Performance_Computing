ecb3d4231e4f1e438fbbe8788235dc348fc20745
joshhamwee <joshhamwee@me.com>
2019-12-05 16:11:16 +0000

    another

--- MPI_Optimisation/stencil.c
+++ MPI_Optimisation/stencil.c
@@ -162,7 +162,7 @@ int main(int argc, char* argv[])
 void stencil(const int start, const int nx, const int ny, const int width, const int height,
              float* image, float* tmp_image)
 {
-  for (int j = 1; j < 1025; ++j) {
+  for (int j = 1; j < 1026; ++j) {
     for (int i = 1; i < 1 + 512; ++i) {
       tmp_image[j + i * height] =  image[j     + i       * height] * 0.6f;
       tmp_image[j + i * height] += (image[j     + (i - 1) * height] + image[j     + (i + 1) * height] + image[j - 1 + i       * height] + image[j + 1 + i       * height])* 0.1f;
